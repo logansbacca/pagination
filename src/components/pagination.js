ef61f13d286d34d72b6707eb45a448e37f028ea0
logan <logans.baca@gmail.comm>
2021-05-22 12:44:45 +0200

    posts

--- src/components/pagination.js
+++ src/components/pagination.js
@@ -3,19 +3,47 @@ import Post from './posts'
 
 function Pagination({ posts, title}) {
 
-    const [dataLimit] = useState(6)
-    const [currentPage] = useState(1);
-    const startIndex = currentPage * dataLimit - dataLimit;
-    const endIndex = startIndex + dataLimit;
-    let slice= posts.slice(startIndex, endIndex);
+    const [postPerPage] = useState(6)
+    const [currentPage,setCurrentPage] = useState(1);
+    const indexOfLastPost = currentPage * postPerPage
+    const indexOfFirstPost = indexOfLastPost - postPerPage
+    let currentPost= posts.slice(indexOfFirstPost,indexOfLastPost);
+
+
+    
+    function goToPreviousPage() {
+      if (currentPage === 1) {
+        setCurrentPage(17)
+      }
+      else {
+        setCurrentPage((currentPost) => currentPost - 1);
+      }
+      
+    }
+
+    function goToNextPage() {
+      if (currentPage === 17) {
+        setCurrentPage(1)
+      }
+      else{
+        setCurrentPage((currentPost) => currentPost + 1);
+      }   
+    }
+    
  
     return (
         <div>
           <h1>{title}</h1>
           <div className="dataContainer">
-            {slice.map((p) => (
+            {currentPost.map((p) => (
               <Post body={p.body} title={p.title} id={p.id}/>
             ))}
+            <nav>
+              <ul>
+              <button onClick={goToPreviousPage}>Previous Page</button>
+              <button onClick={goToNextPage}>Next Page </button>
+              </ul>
+            </nav>
           </div>
         </div>
       );
